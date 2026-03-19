a57bc1bece6b2ab6b34e6075117dd176bbd3517d
David Dudas <david.dudas@outlook.com>
2026-03-20 00:24:09 +0100

    add on board RTC driver and asyncio task

---
+++ firmware/drivers/pcf85063.py
@@ -0,0 +1,64 @@
+from micropython import const
+ 
+# registar overview - crtl & status reg
+RTC_CTRL_1 = const(0x00)
+RTC_CTRL_2 = const(0x01)
+RTC_OFFSET = const(0x02)
+RTC_RAM_by = const(0x03)
+
+# registar overview - time & data reg
+RTC_SECOND_ADDR = const(0x04)
+RTC_MINUTE_ADDR = const(0x05)
+RTC_HOUR_ADDR   = const(0x06)
+RTC_DAY_ADDR    = const(0x07)
+RTC_WDAY_ADDR   = const(0x08)
+RTC_MONTH_ADDR  = const(0x09)
+RTC_YEAR_ADDR   = const(0x0a) # years 0-99; calculate real year = 1970 + RCC reg year
+
+def dectobcd(decimal):
+    """Convert decimal to binary coded decimal (BCD) format"""
+    return (decimal // 10) << 4 | (decimal % 10)
+
+def bcdtodec(bcd):
+    """Convert binary coded decimal to decimal"""
+    return ((bcd >> 4) * 10) + (bcd & 0x0F)
+
+
+class PCF85063:
+    def __init__(self, i2c, addr=0x51):
+        self.i2c = i2c
+        self.addr = addr
+        self._timebuf = bytearray(7) # Pre-allocate a buffer for the time data
+        self._buf = bytearray(1)     # Pre-allocate a single bytearray for re-use
+
+    def datetime(self, datetime=None):
+        """Get or set datetime
+
+        Always sets or returns in 24h format, converts to 24h if clock is set to 12h format
+        datetime : tuple, (0-year, 1-month, 2-day, 3-hour, 4-minutes, 5-seconds, 6-weekday)"""
+        if datetime is None:
+            self.i2c.readfrom_mem_into(self.addr, RTC_SECOND_ADDR, self._timebuf)
+
+            seconds = bcdtodec(self._timebuf[0] & 0x7f)
+            minutes = bcdtodec(self._timebuf[1] & 0x7f)
+            hour    = bcdtodec(self._timebuf[2] & 0x3f)
+            day     = bcdtodec(self._timebuf[3] & 0x3f)
+            weekday = bcdtodec(self._timebuf[4] & 0x07)
+            month   = bcdtodec(self._timebuf[5] & 0x1f)
+            year    = bcdtodec(self._timebuf[6]) + 1970
+
+            return (year, month, day, hour, minutes, seconds, weekday, 0) # Conforms to the ESP8266 RTC (v1.13)
+
+        # Set the clock
+        self._timebuf[0] = dectobcd(datetime[5]) # Seconds
+        self._timebuf[1] = dectobcd(datetime[4]) # Minutes
+        self._timebuf[2] = dectobcd(datetime[3]) # Hour
+        self._timebuf[3] = dectobcd(datetime[2]) # Day
+        self._timebuf[4] = dectobcd(datetime[6]) # Day of week
+        self._timebuf[5] = dectobcd(datetime[1]) # Month
+        self._timebuf[6] = dectobcd(datetime[0] - 1970) # Year
+        
+        self.i2c.writeto_mem(self.addr, RTC_SECOND_ADDR, self._timebuf)
+        return True
+
+
---
+++ firmware/services/rtc_task.py
@@ -0,0 +1,95 @@
+import uasyncio as asyncio
+from machine import Pin, RTC
+import gc
+from logger import Logger
+import time
+from drivers import pcf85063 as pcf85063_driver
+
+# ---- Global variables ----
+import shared_variables as var
+
+log = Logger("rtc", debug_enabled=True)
+
+def is_time_diff_over_threshold(ntp_time, rtc_time, threshold_seconds=60):
+    """
+    ntp_time and rtc_time are tuples like:
+    (year, month, day, hour, minute, second, weekday, subsecond)
+
+    Returns True if the absolute difference is > threshold_seconds (default 60s),
+    otherwise False. If either is None or invalid, logs a warning and returns False.
+    """
+    if ntp_time is None or rtc_time is None:
+        log.warning("NTP or RTC time is None, cannot compare.")
+        return False
+
+    try:
+        # Unpack only the fields we actually need
+        ny, nmo, nd, nh, nmin, ns, _, _ = ntp_time
+        ry, rmo, rd, rh, rmin, rs, _, _ = rtc_time
+
+        # Build time tuples compatible with time.mktime:
+        # (year, month, mday, hour, minute, second, weekday, yearday)
+        # weekday & yearday can be 0, they are usually ignored by mktime.
+        ntp_struct = (ny, nmo, nd, nh, nmin, ns, 0, 0)
+        rtc_struct = (ry, rmo, rd, rh, rmin, rs, 0, 0)
+
+        ntp_seconds = time.mktime(ntp_struct)
+        rtc_seconds = time.mktime(rtc_struct)
+
+        diff = abs(ntp_seconds - rtc_seconds)
+        return diff > threshold_seconds
+
+    except Exception as e:
+        log.warning("Failed to compare times:", ntp_time, rtc_time, "| Error:", e)
+        return False
+
+async def rtc_task(i2c_bus, period = 1.0):
+    #Init
+    pcf85063 = pcf85063_driver.PCF85063(i2c=i2c_bus, addr=0x51)
+    
+    #log.info("Scan result:", i2c_bus.scan())
+    
+    rtc_time = pcf85063.datetime()
+    log.debug(rtc_time)
+    
+    var.system_data.time_rtc = rtc_time
+    
+    if not var.ntp_time_synchronized:
+        rtc = RTC()
+        
+        # MicroPython RTC datetime format:
+        # (year, month, day, weekday, hour, minute, second, subseconds)
+        rtc.datetime((
+            rtc_time[0],  # year
+            rtc_time[1],  # month
+            rtc_time[2],  # day
+            rtc_time[6],  # weekday convert if needed
+            rtc_time[3],  # hour
+            rtc_time[4],  # minute
+            rtc_time[5],  # second
+            0
+        ))
+    
+    #Test to intentionally cause a wrong RTC time
+    #pcf85063.datetime((2022, 10, 9, 11, 11, 11, 0))
+
+    #Run
+    while True:
+        
+        rtc_time = pcf85063.datetime()
+        log.debug(rtc_time)
+        
+        var.system_data.time_rtc = rtc_time
+        
+        log.debug("NTP time synchronized:", var.ntp_time_synchronized)
+        if var.ntp_time_synchronized:
+            log.debug("RTC time:", var.system_data.time_rtc)
+            log.debug("NTP time:", time.localtime())
+            if is_time_diff_over_threshold(time.localtime(), var.system_data.time_rtc, 60):
+                log.warning("RTC time needs to be updated from NTP time!")
+                log.warning("RTC time:", var.system_data.time_rtc)
+                log.warning("NTP time:", time.localtime())
+                pcf85063.datetime(time.localtime())
+            
+        
+        await asyncio.sleep(period)
