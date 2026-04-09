03e4ce9a860f56c28090f4ed77cede9b0005faef
David Dudas <david.dudas@outlook.com>
2026-04-09 13:25:45 +0200

    cleanup unused variables

--- firmware/shared_variables.py
+++ firmware/shared_variables.py
@@ -106,27 +106,6 @@ logger_info = []
 logger_error = []
 logger_warning = []
 
-a = 0
-b = 0
-c = 0
-
-scd41_co2_ppm = 0
-scd41_temp_deg = 0
-scd_41_humidity_percent = 0
-aht21_temp_deg = 0
-aht21_humidity_percent = 0
-battery_voltage = 0
-input_voltage = 0
-analog_lux = 0
-bmp280_pressure = 0
-ens160_aqi = 0
-ens160_tvoc = 0
-ens160_tvoc_rating = 0
-ens160_eco2 = 0
-ens160_eco2_rating = 0
-co2_history = []
-max_history_samples = 60
-
 free_space = 0
 all_space = 0
 
