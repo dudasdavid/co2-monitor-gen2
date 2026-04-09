52c8966f4eb4f588fb559ef444b0b9853b23ef14
David Dudas <david.dudas@outlook.com>
2026-04-09 13:26:15 +0200

    handle sensor screen with LED feedback
    any sensor reading can change feedback

--- firmware/services/led_task.py
+++ firmware/services/led_task.py
@@ -111,6 +111,63 @@ async def led_task(period = 1.0):
                     
                 np.write() # write data to all pixels
             
+            elif not var.selected_alt and var.screen_names[var.current_idx] in ["Sensors"]:
+                
+                if var.led_request_co2 == "Red" or var.led_request_temp == "Red" or var.led_request_hum == "Red":
+                    h = 0
+                elif var.led_request_co2 == "Yellow" or var.led_request_temp == "Yellow" or var.led_request_hum == "Yellow":
+                    h = 48
+                elif var.led_request_co2 == "Blue" or var.led_request_temp == "Blue" or var.led_request_hum == "Blue":
+                    h = 210
+                else:
+                    h = 120
+                                   
+                s = 100
+                
+                v_breath += dir * 1
+                if v_breath > 100:
+                    v_breath = 100
+                    dir = -1
+                elif v_breath < 1:
+                    v_breath = 1
+                    dir = 1
+                    
+                # Ambient lux based LED ring intensity calculation
+                lux = var.sensor_data.lux_veml7700
+                # No animation on the lowest light intensity
+                if lux < 5:
+                    v_breath_scaled = 0.5
+                else:
+                    # v_scaled can be between /3 to /10 based on lux
+                    lux_min = 5.0
+                    lux_max = 100.0
+
+                    # clamp lux into range
+                    lux_clamped = max(lux_min, min(lux, lux_max))
+
+                    # normalize 0..1
+                    t = (lux_clamped - lux_min) / (lux_max - lux_min)
+
+                    # scale goes from 1/10 -> 1/3
+                    scale = (1/10) + t * ((1/3) - (1/10))
+
+                    v_breath_scaled = v_breath * scale
+                
+                # To avoid low yellow turning to red
+                if v_breath_scaled < 0.5:
+                    v_breath_scaled = 0.5
+                    
+                # In pitch black turn off LEDs completely
+                if var.sensor_data.lux_veml7700 < 1:
+                    v_breath_scaled = 0
+                
+                rgb = convert_hsv2rgb(h, s, v_breath_scaled)
+                for i in range(0, len(np)):
+                    np[i] = rgb
+                    
+                np.write() # write data to all pixels
+                
+            
             else:
                 for i in range(0, len(np)):
                     h = phase + i*360.0 / (len(np)-0)
