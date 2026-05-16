c3a88d6b1c436a71d30b352f61b017b585596e76
David Dudas <david.dudas@outlook.com>
2026-05-16 11:49:25 +0200

    immediately start LEDs in boot.py and log any exception to file during startup

--- firmware/boot.py
+++ firmware/boot.py
@@ -3,3 +3,37 @@
 #esp.osdebug(None)
 #import webrepl
 #webrepl.start()
+
+import sys
+LOG_FILE = "/startup_error.log"
+
+def log_exception(exc):
+    try:
+        with open(LOG_FILE, "a") as f:
+            f.write("\n\n=== BOOT ERROR ===\n")
+            sys.print_exception(exc, f)
+            f.write("\n")
+    except:
+        pass
+
+try:
+    from machine import Pin
+    import neopixel
+
+    STARTUP_VALUE = (30, 30, 100)
+    LED_PIN   = const(43)
+    LED_COUNT = const(18)
+
+    pin = Pin(LED_PIN, Pin.OUT)
+    np = neopixel.NeoPixel(pin, LED_COUNT)
+
+    def fill(np, rgb):
+        for i in range(len(np)):
+            np[i] = rgb
+        np.write()
+        
+    fill(np, STARTUP_VALUE)
+        
+except Exception as e:
+    log_exception(e)
+    
--- firmware/main.py
+++ firmware/main.py
@@ -1,9 +1,19 @@
-import uasyncio as asyncio
 import sys
+LOG_FILE = "/startup_error.log"
 
-import shared_variables as var
+def log_exception(exc):
+    try:
+        with open(LOG_FILE, "a") as f:
+            f.write("\n\n=== MAIN ERROR ===\n")
+            sys.print_exception(exc, f)
+            f.write("\n")
+    except:
+        pass
 
 try:
+    import uasyncio as asyncio
+    import shared_variables as var
+
     if var.hw_variant not in ["i80", "spi"]:
         raise OSError("Hardware variant is not selected!")
     if not var.debug:
@@ -13,3 +23,4 @@ except Exception as e:
     # Print error so you can see it in the Thonny shell
     print("FATAL ERROR in main:", e)
     sys.print_exception(e)
+    log_exception(e)
